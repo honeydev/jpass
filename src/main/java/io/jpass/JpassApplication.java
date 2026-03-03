c5ba9f4e888b0921470596ba1933abc6027710f2
honeydev <lebedev.alexey@protonmail.com>
2026-03-03 15:58:18 +1000

    Init project.

---
+++ src/main/java/io/jpass/JpassApplication.java
@@ -0,0 +1,12 @@
+package io.jpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class JpassApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(JpassApplication.class, args);
+	}
+}
